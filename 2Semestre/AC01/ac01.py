16b8d65a046ad8a5cae7376cc24809562ba9ba6e
Viniciustertuliano <41974347+Viniciustertuliano@users.noreply.github.com>
2020-02-24 13:16:36 -0300

    Adicionando função que faz média de dois alunos

--- 2Semestre/AC01/ac01.py
+++ 2Semestre/AC01/ac01.py
@@ -3,23 +3,34 @@
 # Aluno 02: Amanda Conceição Gomes
 # Aluno 03: Vinícius Tertuliano da Silva
 
+
 def adicionar_aluno(alunos, nome, notas):
     if nome in alunos:
         return alunos
     else:
-        alunos[nome]=notas
+        alunos[nome] = notas
     return alunos
+
 
 def remover_aluno(alunos, nome):
     if nome in alunos:
-        alunos.pop(nome)    
-    return alunos 
+        alunos.pop(nome)
+    return alunos
 
-def pesquisar_aluno(alunos, nome):    
+
+def pesquisar_aluno(alunos, nome):
     if nome in alunos:
-        print (alunos[nome])
+        print(alunos[nome])
     else:
-        return print ([])
+        return print([])
+
+def media(atividades):
+     for a in atividades:
+        med = 0
+        for y in range(len(atividades[a])):
+            med += atividades[a][y]
+        med = med / len(atividades[a])
+        return a, med
 
 def calcular_media_turma(alunos):
     s=0
